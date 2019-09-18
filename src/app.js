9a93d1eb8523bfe7247f5d2ab545f34afcfc58c9
Akmal Raza <raza.akmal2298@gmail.com>
2019-09-19 01:11:10 +0530

    set up for heruku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geoCode = require('./utils/geocode.js');
 const forecast = require('./utils/forecast.js');
 
 const app = express()
-
+const port = process.env.PORT || 3000;
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -84,6 +84,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on ' + port )
 })
